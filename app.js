ff8bbbee437967fe33b26d83c3b3179fa82563f8
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-08-08 23:53:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const squares = document.querySelectorAll('.squre');
 const mole = document.querySelector('.mole');
-const timeLeft = document.querySelector('.time-left');
+const timeLeft = document.querySelector('#time-left');
 const score = document.querySelector('#score');
+
+let result = 0;
+
+function randomSquare() {
+    
+}
